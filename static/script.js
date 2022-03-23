aa25802bb8f13d188cd5728a3ae066566f2752d6
George <george.stemmann@gmail.com>
2022-03-22 23:15:57 -0700

    added js extra functions

--- static/script.js
+++ static/script.js
@@ -1,69 +1,69 @@
-'use strict';
+// 'use strict';
 
-let score = 0;
-let words = new Set()
-let time = 120;
+// let score = 0;
+// let words = new Set()
+// let time = 120;
 
-$('#timer').html(time);
-$('form').on('submit', handleSubmit);
+// $('#timer').html(time);
+// $('form').on('submit', handleSubmit);
 
-const makeRequest = () => {
-	axios
-		.post('http://127.0.0.1:5000/')
-		.then((response) => {
-			const newPost = response.data;
-			console.log('POST has been made');
-			appenToDOM([ newPost ]);
-		})
-		.catch((error) => console.error(error));
-};
+// const makeRequest = () => {
+// 	axios
+// 		.post('http://127.0.0.1:5000/')
+// 		.then((response) => {
+// 			const newPost = response.data;
+// 			console.log('POST has been made');
+// 			appenToDOM([ newPost ]);
+// 		})
+// 		.catch((error) => console.error(error));
+// };
 
-$('form').on('submit', handleSubmit);
+// $('form').on('submit', handleSubmit);
 
-async function handleSubmit(e) {
-	e.preventDefault();
-	//this variable is the word submitted through the form
-	let word = $('input').val();
-	//if nothing in form, then do nothing
-	if (!word) return;
-	//send word in form to server to "check_valid_word" method in boggle.py
+// async function handleSubmit(e) {
+// 	e.preventDefault();
+// 	//this variable is the word submitted through the form
+// 	let word = $('input').val();
+// 	//if nothing in form, then do nothing
+// 	if (!word) return;
+// 	//send word in form to server to "check_valid_word" method in boggle.py
 
-	const res = await axios.get('/check-word', {
-		params : {
-			word : word
-		}
-	});
-	let response = res.data.response;
-	let new_response = document.getElementById('response');
-	new_response.append(response);
-	$('form').trigger('reset');
+// 	const res = await axios.get('/check-word', {
+// 		params : {
+// 			word : word
+// 		}
+// 	});
+// 	let response = res.data.response;
+// 	let new_response = document.getElementById('response');
+// 	new_response.append(response);
+// 	$('form').trigger('reset');
 
-	if (response === 'ok') {
-		if (words.has(word)) {
-			return;
-		}
-		words.add(word);
-		score += word.length;
-		$('#score').html(`Score: ${score}`);
-	}
-}
-let countDown = setInterval(() => {
-	time--;
-	$('#timer').html(time);
-	stopTimer();
-}, 1000);
+// 	if (response === 'ok') {
+// 		if (words.has(word)) {
+// 			return;
+// 		}
+// 		words.add(word);
+// 		score += word.length;
+// 		$('#score').html(`Score: ${score}`);
+// 	}
+// }
+// let countDown = setInterval(() => {
+// 	time--;
+// 	$('#timer').html(time);
+// 	stopTimer();
+// }, 1000);
 
-function stopTimer() {
-	if (time < 1) {
-		clearInterval(countDown);
-		$('form').hide();
-		$('.container').append($('<span>').html('GAME OVER'));
-		endgame();
-	}
-}
-async function endGame() {
-	await axios.post('/end-game', {score : score})
-}
+// function stopTimer() {
+// 	if (time < 1) {
+// 		clearInterval(countDown);
+// 		$('form').hide();
+// 		$('.container').append($('<span>').html('GAME OVER'));
+// 		endgame();
+// 	}
+// }
+// async function endGame() {
+// 	await axios.post('/end-game', {score : score})
+// }
 
 
 // "use strict";
@@ -126,3 +126,104 @@ async function endGame() {
 //  //post score to server to see if the high score needs to be updated
 //  await axios.post("/end-game", { score: score });
 // }
+
+
+class BoggleGame {
+	/* make a new game at this DOM id */
+  
+	constructor(boardId, secs = 60) {
+	  this.secs = secs; // game length
+	  this.showTimer();
+  
+	  this.score = 0;
+	  this.words = new Set();
+	  this.board = $("#" + boardId);
+  
+	  // every 1000 msec, "tick"
+	  this.timer = setInterval(this.tick.bind(this), 1000);
+  
+	  $(".add-word", this.board).on("submit", this.handleSubmit.bind(this));
+	}
+  
+	/* show word in list of words */
+  
+	showWord(word) {
+	  $(".words", this.board).append($("<li>", { text: word }));
+	}
+  
+	/* show score in html */
+  
+	showScore() {
+	  $(".score", this.board).text(this.score);
+	}
+  
+	/* show a status message */
+  
+	showMessage(msg, cls) {
+	  $(".msg", this.board)
+		.text(msg)
+		.removeClass()
+		.addClass(`msg ${cls}`);
+	}
+  
+	/* handle submission of word: if unique and valid, score & show */
+  
+	async handleSubmit(evt) {
+	  evt.preventDefault();
+	  const $word = $(".word", this.board);
+  
+	  let word = $word.val();
+	  if (!word) return;
+  
+	  if (this.words.has(word)) {
+		this.showMessage(`Already found ${word}`, "err");
+		return;
+	  }
+  
+	  // check server for validity
+	  const resp = await axios.get("/check-word", { params: { word: word }});
+	  if (resp.data.result === "not-word") {
+		this.showMessage(`${word} is not a valid English word`, "err");
+	  } else if (resp.data.result === "not-on-board") {
+		this.showMessage(`${word} is not a valid word on this board`, "err");
+	  } else {
+		this.showWord(word);
+		this.score += word.length;
+		this.showScore();
+		this.words.add(word);
+		this.showMessage(`Added: ${word}`, "ok");
+	  }
+  
+	  $word.val("").focus();
+	}
+  
+	/* Update timer in DOM */
+  
+	showTimer() {
+	  $(".timer", this.board).text(this.secs);
+	}
+  
+	/* Tick: handle a second passing in game */
+  
+	async tick() {
+	  this.secs -= 1;
+	  this.showTimer();
+  
+	  if (this.secs === 0) {
+		clearInterval(this.timer);
+		await this.scoreGame();
+	  }
+	}
+  
+	/* end of game: score and update message. */
+  
+	async scoreGame() {
+	  $(".add-word", this.board).hide();
+	  const resp = await axios.post("/post-score", { score: this.score });
+	  if (resp.data.brokeRecord) {
+		this.showMessage(`New record: ${this.score}`, "ok");
+	  } else {
+		this.showMessage(`Final score: ${this.score}`, "ok");
+	  }
+	}
+  }
--- test.py
+++ test.py
@@ -6,19 +6,19 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    def setUp(self):
+   def setUp(self):
        """Stuff to do before every test"""
  
        self.client = app.test_client()
        app.config['TESTING'] = True
 
-    def test_homepage(self):
+   def test_homepage(self):
        """check to make sure everything starts up correctly with the displays we are hoping for"""
        with self.client:
            res = self.client.get('/')
            html = res.get_data(as_text=True)
 
-    def test_homepage(self):
+   def test_homepage(self):
        with self.client:
            res = self.client.get('/')
            decoded = res.data.decode()
@@ -27,14 +27,38 @@ class FlaskTests(TestCase):
            self.assertIn("High Score: 0", decoded)
 
 
-    def test_homepage(self):
+   def test_homepage(self):
        """check to make sure everything starts up correctly with the displays we are hoping for"""
        with self.client:
            res = self.client.get('/')
            decoded = res.data.decode()
-           self.assertIn("Score: 0", decoded)
            self.assertIn("Times Played: 0", decoded)
            self.assertIn("High Score: 0", decoded)
            self.assertIn('board',session)
            self.assertIsNone(session.get("highscore"))
            self.assertIsNone(session.get("numplays"))
+           
+   def test_valid_word(self):
+
+         with self.client as client:
+            with client.session_transaction() as sess:
+               sess['board'] = [["R", "O", "K", "R", "T"],
+                                ["C", "C", "T", "T", "T"],
+                                ["C", "A", "T", "T", "T"],
+                                ["C", "A", "T", "T", "T"],
+                                ["C", "A", "T", "T", "T"]]
+            response = self.client.get('/check-word?word=rock')
+            self.assertEqual(response.json['response'], 'ok')
+
+   def test_not_word(self):
+       """test if a non-english word is submitted"""
+      
+       with self.client as client:
+           with client.session_transaction() as sess:
+               sess['board'] = [["R", "O", "K", "R", "T"],
+                                ["C", "C", "T", "T", "T"],
+                                ["C", "A", "T", "T", "T"],
+                                ["C", "A", "T", "T", "T"],
+                                ["C", "A", "T", "T", "T"]]
+       response = self.client.get('/check-word?word=leblanc')
+       self.assertEqual(response.json['response'], 'not-word')
